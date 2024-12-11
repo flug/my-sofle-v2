d69fa63ccf1ef10ce5d37c41f9ef02f17afddb06
Flug <flugv1@gmail.com>
2024-12-11 22:50:33 +0100

    changement de la fonction

--- default/keymap.h
+++ default/keymap.h
@@ -416,13 +416,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
     switch (get_highest_layer(state)) {
         case 0:
-            oled_write_ln_P(layer_icons[_DEFAULT], _IMAGE_SIZE);
+            oled_write_raw_P(layer_icons[_DEFAULT], _IMAGE_SIZE);
             break;
         case 1:
-            oled_write_ln_P(layer_icons[_PERCENT], _IMAGE_SIZE);
+            oled_write_raw_P(layer_icons[_PERCENT], _IMAGE_SIZE);
             break;
         case 4:
-            oled_write_ln_P(layer_icons[_CODE], _IMAGE_SIZE);
+            oled_write_raw_P(layer_icons[_CODE], _IMAGE_SIZE);
             break;
         default:
             oled_write_ln_P(PSTR("Inconnu"), false);
