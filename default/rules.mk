e930d6974cdbcd80d08ac07bbd1a5a7d9d457102
Flug <flugv1@gmail.com>
2025-02-28 01:11:35 +0100

    test leds

--- default/rules.mk
+++ default/rules.mk
@@ -1,4 +1,5 @@
 AUDIO_ENABLE = no
 SPLIT_KEYBOARD = yes
 SPLIT_USB_DETECT = yes
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = yes # Enables QMK's RGB code  
+WS2812_DRIVER = vendor # Use the RP2040's PIO interface
